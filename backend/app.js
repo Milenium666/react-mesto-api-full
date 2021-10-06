eed262746b642a6efe9f5909b5e07382e41ee8de
Milenium666 <whiteangel64rus@gmail.com>
2021-10-06 09:02:53 +0000

    добавыить Краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
