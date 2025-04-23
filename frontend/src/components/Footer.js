cb39dfb4d76daae469fe805aa65a960ca2b816ec
Milenium666 <whiteangel64rus@gmail.com>
2025-04-23 16:26:30 +0300

    fix: upd years at footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return(
         <footer className="footer indent">
-      <p className="footer__copyright">&#169; 2021 Mesto Russia</p>
+      <p className="footer__copyright">&#169; 2022 - 2025 Mesto Russia</p>
   </footer>
     )
 }
